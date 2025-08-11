c681ac5c50989fdf88ab7901591720e2da41c49d
Wang Zhiyu <david-ajax@outlook.com>
2025-08-12 02:43:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -198,7 +198,7 @@ function waGetInquirer() {
         INQUIRER="/tmp/waInquirer.sh"
         rm -f "$INQUIRER"
 
-        curl -o "$INQUIRER" "https://raw.githubusercontent.com/winapps-org/winapps/main/install/inquirer.sh"
+        curl -o "$INQUIRER" "https://raw.githubusercontent.com/david-ajax/winapps-no-app-detect/main/install/inquirer.sh"
     fi
 
     # shellcheck source=/dev/null # Exclude this file from being checked by ShellCheck.
